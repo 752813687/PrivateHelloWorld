f2e3e9077487d483b3623de8daa946c24c7ebfdd
zhulei <752813687@qq.com>
2020-07-13 10:11:51 +0800

    测试

--- Example/PrivateHelloWorld/ZLViewController.m
+++ Example/PrivateHelloWorld/ZLViewController.m
@@ -20,7 +20,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     [PrintHelloWorld printHelloWorld];
-    NSLog(@"xxxx");
+    NSLog(@"xxiiiix");
 }
 
 - (void)didReceiveMemoryWarning
