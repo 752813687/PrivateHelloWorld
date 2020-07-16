532da51b20bf13ec04f2ea3bcb4f68e7ab1d6b1a
zhulei <752813687@qq.com>
2020-07-16 14:24:35 +0800

    建设大街

--- Example/PrivateHelloWorld/ZLViewController.m
+++ Example/PrivateHelloWorld/ZLViewController.m
@@ -20,7 +20,10 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     [PrintHelloWorld printHelloWorld];
-   
+    NSLog(@"xxiiiix");
+    NSLog(@"xxiiiix");
+    NSLog(@"xxiiiix");
+
 }
 
 - (void)didReceiveMemoryWarning
