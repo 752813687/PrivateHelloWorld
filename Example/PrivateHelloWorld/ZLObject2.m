b966ea73907a7c681446b3eab4a908398bc096ab
zhulei <752813687@qq.com>
2021-02-23 11:05:56 +0800

    2.12文件增加

---
+++ Example/PrivateHelloWorld/ZLObject2.m
@@ -0,0 +1,13 @@
+//
+//  ZLObject2.m
+//  PrivateHelloWorld_Example
+//
+//  Created by Jack on 2021/2/23.
+//  Copyright © 2021 752813687. All rights reserved.
+//
+
+#import "ZLObject2.h"
+
+@implementation ZLObject2
+
+@end
