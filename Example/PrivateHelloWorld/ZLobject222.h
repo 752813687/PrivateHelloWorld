2ddfd6de714c8b082463d57d202f89f534c96631
zhulei <752813687@qq.com>
2021-02-23 11:15:00 +0800

    2222增加文件

---
+++ Example/PrivateHelloWorld/ZLobject222.h
@@ -0,0 +1,17 @@
+//
+//  ZLobject222.h
+//  PrivateHelloWorld_Example
+//
+//  Created by Jack on 2021/2/23.
+//  Copyright © 2021 752813687. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZLobject222 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
